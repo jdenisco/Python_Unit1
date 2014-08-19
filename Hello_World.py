025802b366148233ddb846a4df4511a44323d711
James Denisco <james.denisco@genesys.com>
2014-08-19 17:38:37 -0400

    Getting started

---
+++ Hello_World.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+#   file: Hello_World
+#   date: 2014-08-13
+#   author: jdenisco
+#   email: james.denisco@genesys.com
+#
+# Copyright © 2014 jdenisco <james.denisco@genesys.com>
+#
+
+"""
+Description:
+"""
+
+import sys
+
+print "Hello World!"
+python_version = sys.version
+print "This program is running: " + python_version
+print "That's all. Goodbye!"
+
